e07a5bdfe30a437e98acaba6f05919578090f98f
Himanshu016-hub <56622550+Himanshu016-hub@users.noreply.github.com>
2021-10-05 22:44:30 +0530

    whatsapp clone day1 code

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBniDNYq_BRiQKO4nzHP82qSNpFdg4aXaE",
+    authDomain: "whats-app-clone-bb12f.firebaseapp.com",
+    projectId: "whats-app-clone-bb12f",
+    storageBucket: "whats-app-clone-bb12f.appspot.com",
+    messagingSenderId: "456966203762",
+    appId: "1:456966203762:web:dcd39e021eb93df33755eb",
+    measurementId: "G-GC1290PNB5"
+  };
